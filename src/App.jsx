521eb65a30a4a1b58fe2cc1b2cb1ad696dde57f0
Anshul <anshuls26@gmail.com>
2025-06-21 18:20:01 +0530

    update font-family and black background after animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 function App() {
+  let [showContent, setShowContent] = useState(false)
+
+  useGSAP(()=>{
+    const tl = gsap.timeline();
+    tl.to(".vi-mask-group", {
+      rotate: 50,
+      ease: "Power4.easeInOut",
+      duration: 2,
+      transformOrigin: "50% 50%",
+    })
+    .to(".vi-mask-group", {
+      scale: 10,
+      duration: 2,
+      delay: -1.9,
+      ease:"Expo.easeInOut",
+      transformOrigin: "50% 50%",
+      opacity: 0,
+      onUpdate: function(){
+        if(this.progress() >= .9){
+          document.querySelector(".svg").remove()
+          setShowContent(true)
+          this.kill()
+        }
+      }
+    })
+  })
   return (
+    <>
     <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
     <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
       <defs>
@@ -31,6 +60,10 @@ function App() {
       />
     </svg>
   </div>
+  {showContent && <div className="main w-full">
+    <div className='landing w-full bg-black h-screen'></div>
+    </div>}
+  </>
   )
 }
 
